5aac0bf2dcc261f44ba69ac36254d235c9be789c
Tayna Lima <limatayna115@gmail.com>
2025-09-16 20:23:44 +0000

    aula da questao toalha

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,19 @@
+print("questão toalha")
+
+class Towel:
+    def __init__(self, color: str, size: str):
+        self.color = "red"
+        self.size = "p"
+        self.wetness = 0
+    def __str__(self):
+        return f"color:{self.color}, tam:{self.size}, hum:{self.wetness}"
+
+towel = Towel()
+print(towel.color)
+towel.color = 'white'
+print(towel.color)
+print(towel.size)
+print(towel.wetness)
+
+
+                 
