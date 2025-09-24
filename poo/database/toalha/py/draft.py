1c833db945b50352a9c9032af7b404f8172eea61
lftayna <limatayna115@gmail.com>
2025-09-24 20:49:44 -0300

    fazendo a main junto ao professor

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -19,9 +19,33 @@ class Towel:
          return 30
         return 0 
 
+    def wringOut(self) -> None:
+        self.wetness = 0
+
+    def isDry(self) -> bool:
+        return self.wetness == 0
 
     def __str__(self) -> str: 
         return f"Cor: {self.color}, Tam: {self.size}, Umidade: {self.wetness}"
+
+    def main():
+        toalha = Towel("", "") #obj manipulado
+        while True: #looping infinito
+            line:str = input() #entrada da linha
+            args: list[str] = line.split("") #6 linha de palavras
+            if args[0] == "end": #fim da execução
+                break 
+            elif args[0] == "new" #color size 
+            color = args[1]
+            size = args[2]
+            toalha = Towel(color, size)
+            elif args[0] == "show":
+                print(toalha)
+            elif args[0] == "dry": #amount 
+                amount: int == int(args[1])
+            toalha.dry(amount)
+            else:
+                print("fail")
 
 Minhatoalha: str = Towel("rosa", "M")
 print(Minhatoalha)
@@ -29,4 +53,6 @@ Minhatoalha.dry(3)
 print(Minhatoalha)
 Minhatoalha.dry(15)
 Minhatoalha.dry(10)
-print(Minhatoalha)
+print(Minhatoalha) 
+
+
