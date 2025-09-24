9024eaeade9a4c2fc680c543dadd5b134de75c65
lftayna <limatayna115@gmail.com>
2025-09-24 09:48:59 -0300

    tentando entender o codigo

--- poo/database/toalha/py/draft.py
+++ poo/database/toalha/py/draft.py
@@ -1,54 +1,32 @@
-print("questão toalha")
-
 class Towel:
-    def __init__(self, color: str, size: str)
-    self.color: str = color 
-    self.size: str = size
-    self.wetness: int = 0
+    def __init__(self, color: str, size: str):
+        self.color: str = color
+        self.size: str = size 
+        self.wetness: int = 0
 
-    def dry(self, amount: int) -> None:
+    def dry(self,  amount: int) -> None:
         self.wetness += amount 
         if self.wetness >= self.getMaxWetness():
-            print("toalha enxarcada")
-            self.wetness = self.getMaxWetness()
+            print("toalha encharcada")
+            self.wetness = self.getMaxWetness() #pq não tem : aqui? cadê o else?
 
-     def isDry(self) -> bool:
-        return self.wetness == 0
-    
-    def wringOut(self) -> None:
-        self.wetness = 0
-
-    def getMaxWetness(self) -> int:
+    def getMaxWetness(self) -> int: 
         if self.size == "P":
-            return 10
+         return 10
         if self.size == "M":
-            return 20
+         return 20
         if self.size == "G":
-            return 30
-        return 0
+         return 30
+        return 0 
 
-    def __str__(self) -> str: # toString
-        return f"Cor:{self.color}, Tam:{self.size}, Umidade:{self.wetness}"
 
-def main():
-    toalha = Towel("", "") #3. obj manipulado
-    while True: # loop infinito
-        line: str = input() #entrada da linha
-        args: list[str] = line.split("") #6 linha de palavras
-        if args[0] == "end": #fim da execução
-            break 
+    def __str__(self) -> str: 
+        return f"Cor: {self.color}, Tam: {self.size}, Umidade: {self.wetness}"
 
-        elif args[0] == "new" # color size
-        color = args[1]
-        size = args[2]
-        toalha = Towel (color, size)
-        elif args[0] == "show":
-            print(toalha)
-        elif args[0] == "dry": #mount
-            amount: int = int(args[1])
-            toalha.dry(amount)
-            else: 
-                print("fail")
- 
-
-                 
+Minhatoalha: str = Towel("rosa", "M")
+print(Minhatoalha)
+Minhatoalha.dry(3)
+print(Minhatoalha)
+Minhatoalha.dry(15)
+Minhatoalha.dry(10)
+print(Minhatoalha)
