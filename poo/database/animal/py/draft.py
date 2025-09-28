c1ea703bd72235b8dbded7585466d6f70290acaf
lftayna <limatayna115@gmail.com>
2025-09-28 15:08:24 -0300

    fiz a main e testes

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -12,16 +12,31 @@ class Animal:
         if self.age > 0 and self.age < 4:
             print(self.sound)
     
-    def ageBy(self, increment: int) -> int:
-        self.age += increment
-        self.age >= MaxAge()
-        print("warning {species} morreu")
-        self.age = MaxAge()
+    def ageBy(self, increment: int) -> None:
+        self.age += increment 
+        if self.age > 4:
+            self.age = 4
 
     def __str__(self) -> str:
-        return f("")
+        return f"{self.species}:{self.age}:{self.sound}"
 
-
-meuAnimal = Animal("cachorro", "auau") 
-print(meuAnimal)
-
+def main():
+    animal = Animal(" ", " ")
+    while True:
+        line: str = input()
+        print("$" + line)
+        args: list[str] = line.split(" ")
+        if args[0] == "end":
+            break
+        elif args[0] == "init":
+            species = args[1]
+            sound = args[2]
+            animal = Animal(species, sound)
+        elif args[0] == "show":
+            print(animal)
+        elif args[0] == "grow":
+            increment: int = int(args[1])
+            animal.ageBy(increment)
+            if animal.age == 4:
+                print(f"warning: {animal.species} morreu")
+main()
