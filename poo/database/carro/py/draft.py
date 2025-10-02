685f24e9ce1741256feacce6dd3a25bd6acaef12
lftayna <limatayna115@gmail.com>
2025-10-02 08:37:26 -0300

    commit da atividade

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -1,15 +1,15 @@
 class Carro:
-    def __init__(self, pas: int, km: int, gas: int):
+    def __init__(self, passageiros: int, km: int, gas: int):
         self.passageiros: int = 0 
         self.km: int = 0
         self.gas: int = 0
-        self.passMax: int = 2 
+        self.passMax = 2
         self.gasMax: int = 100
-
+        
     def enter(self) -> None:
         if self.passageiros < self.passMax:
             self.passageiros += 1
-        else:
+        else: 
             print("fail: limite de pessoas atingido")
 
     def leave(self) -> None:
@@ -17,8 +17,29 @@ class Carro:
             self.passageiros -= 1
         else:
             print("fail: nao ha ninguem no carro")
+        
+    def fuel(self, increment: int):
+        if self.gas < 100:
+            self.gas += increment
+            if self.gas > 100:
+                self.gas = 100
 
-
+    def drive(self, distance: int) -> None:
+        if self.passageiros == 0:
+            print("fail: nao ha ninguem no carro")
+            return
+        if self.gas == 0:
+            print("fail: tanque vazio")
+            return
+        if distance <= self.gas:
+            self.km += distance
+            self.gas -= distance
+        else:
+            kmpercorrido = self.gas
+            self.km += kmpercorrido
+            self.gas = 0
+            print(f"fail: tanque vazio apos andar {kmpercorrido} km")
+            
     def __str__(self) -> str:
         return f"pass: {self.passageiros}, gas: {self.gas}, km: {self.km}"
 
@@ -35,6 +56,12 @@ def main():
         if args[0] == "enter":
             carro.enter()
         if args[0] == "leave":
-            carro.leave()
+            carro.leave() 
+        if args[0] == "fuel":
+            increment: int = int(args[1])
+            carro.fuel(increment)
+        if args[0] == "drive":
+            distance: int = int(args[1])
+            carro.drive(distance)
 
 main()
