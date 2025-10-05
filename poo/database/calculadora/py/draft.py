8e7a2d4da1e37e1bff84763460b048adbdad9a36
lftayna <limatayna115@gmail.com>
2025-10-05 11:47:38 -0300

    calculadora feita sozinha

--- poo/database/calculadora/py/draft.py
+++ poo/database/calculadora/py/draft.py
@@ -1,29 +1,59 @@
-class Calculadora: 
-    def __init__(self, battery: int, batteryMax: int, display: int):
+class Calculadora:
+    def __init__(self, batteryMax: int):
+        self.display: float = 0
+        self.batteryMax = batteryMax
         self.battery: int = 0
-        self.batteryMax: int = 0
-        display: int = 0.00
-    
+
+    def charge(self, value) -> None:
+        if self.battery < self.batteryMax:
+            self.battery += value
+        if self.battery > self.batteryMax:
+            self.battery = self.batteryMax
+
     def __str__(self) -> str:
-        return f"pass: display = {display:.2f}, battery = {battery}"
+        return f"display = {self.display:.2f}, battery = {self.battery}"
 
-    def calculator(self, batteryMax: int):
-        print(batteryMax)
-    
-    def change(self, increment: int) -> None:
+    def sum(self, a: int, b: int) -> None:
+        if self.battery == 0:
+            print(f"fail: bateria insuficiente")
+            return
+        else: 
+            self.display = a + b
+            self.battery -= 1
 
-    def 
-def main(): 
-    calculadora = Calculadora
+    def div(self, num: int, den: int):
+        if self.battery == 0:
+            print("fail: bateria insuficiente")
+            return
+        if den == 0:
+            print("fail: divisao por zero")
+            self.battery -= 1    
+        else: 
+            self.display = num / den
+            self.battery -= 1
+
+def main():
+    calculdora = None
     while True:
         line: str = input()
-        print('$' + line)
-        args: list[str] = line.split(" ")
+        print("$" + line)
+        args: list[str] = line.split()
         if args[0] == "end":
             break
         if args[0] == "init":
-            print("init" + batteryMax)
+            calculadora = Calculadora(int(args[1]))
         if args[0] == "show":
-            calculadora.__str__()
-        
-        
+            print(calculadora)
+        if args[0] == "charge":
+            value: int = int(args[1])
+            calculadora.charge(value)
+        if args[0] == "sum":
+            a: int = int(args[1])
+            b: int = int(args[2])
+            calculadora.sum(a, b)
+        if args[0] == "div":
+            num: int = int(args[1])
+            den: int =int(args[2])
+            calculadora.div(num, den)
+
+main()
