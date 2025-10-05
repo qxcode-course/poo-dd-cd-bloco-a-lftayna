1ba1fe3fbcc31e19992612618a5721df1f491e18
lftayna <limatayna115@gmail.com>
2025-10-05 01:33:58 -0300

    refazendo toalha sozinha

--- poo/database/enxugar/py/draft.py
+++ poo/database/enxugar/py/draft.py
@@ -1,17 +1,19 @@
-class Towel: 
-    def __init__(self, color:str, size: str):
+class Towel:
+    def __init__(self, color: str, size: str):
         self.color: str = color
         self.size: str = size
-        self.wetness: int = 0
+        self.wetness: int = 0 #isso não é um parametro, e sim uma variavel, o restante são parametros.
 
-    def dry(self, amount: int) -> None:
-        self.wetness += amount 
+    def dry(self, amount: int):
+        self.wetness += amount
         if self.wetness >= self.getMaxwetness():
             print("toalha encharcada")
             self.wetness = self.getMaxwetness()
-    
 
-    def getMaxwetness(self) -> int: 
+    def wringOut(self) -> None:
+        self.wetness = 0
+
+    def getMaxwetness(self) -> int:
         if self.size == "P":
             return 10
         if self.size == "M":
@@ -20,36 +22,34 @@ class Towel:
             return 30
         return 0
 
-    def wringOut(self) -> None: 
-        self.wetness = 0
+    def isDry(self) -> bool:
+        if self.wetness == 0:
+            return True
+        else: 
+            return False
 
-    def isDry(self) -> bool: 
-            return self.wetness == 0
-    
-    def __str__(self) -> str: 
+    def __str__(self) -> str:
         return f"Cor: {self.color}, Tamanho: {self.size}, Umidade: {self.wetness}"
 
 def main():
-    minhaToalha = Towel(" ", " ")
-    while True: 
-        line: str = input()
-        print("$" + line)
-        args: list[str] = line.split(" ")
-        if args[0] == "end":
-            break
-        elif args[0] == "criar":
-            color = args[1]
-            size = args[2]
-            minhaToalha = Towel(color, size)
-        elif args[0] == "mostrar":
-            print(minhaToalha)
-        elif args[0] == "torcer":
-            minhaToalha.wringOut()
-        elif args[0] == "seca":
-            print("sim" if minhaToalha.isDry() else "nao")
-        elif args[0] == "enxugar":
-              amount: int = int(args[1])
-              minhaToalha.dry(amount)
-        else: 
-            print("fail: codigo invalido")
+        toalha = Towel(" ", " ")
+        while True:
+            line: str = input()
+            print("$" + line)
+            args: list[str] = line.split(" ")
+            if args[0] == "end":
+                break
+            if args[0] == "criar":
+                color = args[1]
+                size = args[2]
+                toalha = Towel(color, size)
+            if args[0] == "mostrar":
+                print(toalha)
+            if args[0] == "seca":
+                print(f"sim" if toalha.isDry() else "nao")
+            if args[0] == "enxugar":
+                amount: int = int(args[1])
+                toalha.dry(amount)
+            if args[0] == "torcer":
+                toalha.wringOut()
 main()
