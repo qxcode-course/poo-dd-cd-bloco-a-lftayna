f60916bd57d4d776e9791919e313206d3c06dc16
lftayna <limatayna115@gmail.com>
2025-10-05 11:51:12 -0300

    animal

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -4,41 +4,42 @@ class Animal:
         self.sound: str = sound
         self.age: int = 0
 
-    def makeSound(self): 
+    def makeSound(self):
         if self.age == 0:
-            print("---") 
+            print('---')
         if self.age >= 4:
             print("RIP")
         if self.age > 0 and self.age < 4:
             print(self.sound)
-    
-    def ageBy(self, increment: int) -> None:
-        self.age += increment 
-        if self.age > 4:
-            self.age = 4
 
+    def ageBy(self, increment: int) -> None:
+        self.age += increment
+        if self.age >= 4:
+            print(f"warning: {self.species} morreu")
+            self.age = 4
+           
     def __str__(self) -> str:
         return f"{self.species}:{self.age}:{self.sound}"
 
 def main():
-    animal = Animal(" ", " ")
+    animal = Animal
     while True:
         line: str = input()
         print("$" + line)
         args: list[str] = line.split(" ")
         if args[0] == "end":
             break
-        elif args[0] == "init":
+        if args[0] == "init":
             species = args[1]
             sound = args[2]
             animal = Animal(species, sound)
-        elif args[0] == "show":
+        if args[0] == "show":
             print(animal)
-        elif args[0] == "grow":
+        if args[0] == "noise":
+            animal.makeSound()
+        if args[0] == "grow":
             increment: int = int(args[1])
             animal.ageBy(increment)
-            if animal.age == 4:
-                print(f"warning: {animal.species} morreu")
-        elif args[0] == "noise":
-            animal.makeSound()
 main()
+
+
